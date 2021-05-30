04aa413527c011139fd32b4e704e6624571c7bd2
Elías <eliasfb94@hotmail.com>
2021-05-30 12:02:28 +0200

    Enhanced python scrapper

--- src/assets/mocks/bgnGameScrapper.py
+++ src/assets/mocks/bgnGameScrapper.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 
 def save_request_on_file(request_name, file_name):
@@ -9,9 +10,7 @@ def save_request_on_file(request_name, file_name):
     file.close()
 
 
-gameIds = [1, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33,
-           34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 58, 60, 62, 63,
-           64, 65, 66, 67, 68, 70, 71, 72, 73, 75, 76, 102, 103, 104, 105, 110, 111, 112, 113, 114, 115, 116, 118]
+gameIds = json.loads(requests.get("http://localhost:8080/games/ids").text)
 save_request_on_file("games", "games")
 save_request_on_file("masters", "masters")
 save_request_on_file("constants", "constants")
