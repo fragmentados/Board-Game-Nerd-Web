1f6e11f7c6d439368078187e2d9a21b32746a709
Elías <eliasfb94@hotmail.com>
2021-05-30 12:26:08 +0200

    Fix backend mocked

--- src/app/models/service.ts
+++ src/app/models/service.ts
@@ -3,4 +3,4 @@ export const BACKEND_URL = 'http://' + BACKEND_IP + ':8080/';
 export const UNIQUE_CONSTRAINT_CODE = -2;
 export const OK_CODE = 0;
 export const DEFAULT_LANG = 'es';
-export const BACKEND_MOCKED = false;
+export const BACKEND_MOCKED = true;
---
+++ src/assets/mocks/release.bat
@@ -0,0 +1,4 @@
+python bgnGameScrapper.py
+git add -A
+git commit -m "Release"
+git push origin master
